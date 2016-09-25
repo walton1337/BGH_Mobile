21a71dbf0739f30e1afa85e1789381dd3c83bf3c
waltonlee <walton.lee@tufts.edu>
2016-09-25 13:49:33 -0400

    initialized repo

---
+++ app/src/androidTest/java/walton1ee7/github/io/bgh_mobile/ApplicationTest.java
@@ -0,0 +1,13 @@
+package walton1ee7.github.io.bgh_mobile;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
